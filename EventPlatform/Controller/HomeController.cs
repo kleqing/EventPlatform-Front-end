87a574be41a1cf9d6930576fec822198fa2b6750
Thanh DT <thanhdt969@gmail.com>
2025-10-15 07:55:03 +0700

    Add Login

---
+++ EventPlatform/Controller/HomeController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventPlatform.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: HomeController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // ✅ Thêm phần này để test UI login
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View(); // Hiển thị trang Views/Home/Login.cshtml
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(string username, string password)
+        {
+            // Chỉ để test UI, chưa xử lý đăng nhập thật
+            if (username == "admin" && password == "123")
+            {
+                ViewBag.Message = "Login successful!";
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Error = "Invalid username or password!";
+            return View();
+        }
+
+        // Các action còn lại giữ nguyên...
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
