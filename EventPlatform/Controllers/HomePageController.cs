b8bdedbe5d115766d8fe3ed391d7c915f60f3f94
thanhdt12k <thanhdt969@gmail.com>
2025-10-18 20:34:27 +0700

    Thanh (#4)
    * [Update] Ignore .user profile
    
    * Update UI Event, Event Detail, HomePage, Profile
    
    * Add Forum, Homepage, Profile

---
+++ EventPlatform/Controllers/HomePageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventPlatform.Controllers
+{
+    public class HomePageController : Controller
+    {
+        public IActionResult HomePage()
+        {
+            return View();
+        }
+    }
+}
