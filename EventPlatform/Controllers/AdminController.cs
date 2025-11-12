d79f34ff6161a41336514abed40647512852ab27
Thanh DT <thanhdt969@gmail.com>
2025-11-12 07:16:43 +0700

    Add admin UI

---
+++ EventPlatform/Controllers/AdminController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventPlatform.Controllers
+{
+    [Route("admin")] 
+    public class AdminController : Controller
+    {
+        [Route("")] 
+        [Route("index")] 
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Dashboard";
+            return View();
+        }
+
+        [Route("events")] 
+        public IActionResult Events()
+        {
+            ViewData["Title"] = "Event Management";
+            return View();
+        }
+
+        [Route("users")] 
+        public IActionResult Users()
+        {
+            ViewData["Title"] = "User Management";
+            return View();
+        }
+
+        [Route("transactions")] 
+        public IActionResult Transactions()
+        {
+            ViewData["Title"] = "Transaction History";
+            return View();
+        }
+
+        [Route("speakers")]
+        public IActionResult Speakers()
+        {
+            ViewData["Title"] = "Speaker Applications";
+            return View();
+        }
+
+        [Route("categories")] 
+        public IActionResult Categories()
+        {
+            ViewData["Title"] = "Manage Categories";
+            return View();
+        }
+    }
+}
